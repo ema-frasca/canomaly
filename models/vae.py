f18c66147cb1f7110d4b8106f640544d977dcd76
ema-frasca <60889740+ema-frasca@users.noreply.github.com>
2022-01-28 15:11:46 +0100

    updated vae

--- models/vae.py
+++ models/vae.py
@@ -5,10 +5,11 @@ from argparse import Namespace, ArgumentParser
 from models import CanomalyModel
 from backbones import get_encoder, get_decoder
 from datasets.utils.canomaly_dataset import CanomalyDataset
+import math
 
 
 class VAE(CanomalyModel):
-    NAME = 'VAE'
+    NAME = 'vae'
     VARIATIONAL = False
     CONDITIONAL = False
 
@@ -17,7 +18,7 @@ class VAE(CanomalyModel):
         parser.add_argument('--latent_space', type=int, required=True,
                             help='Latent space dimensionality.')
         parser.add_argument('--beta_kl', type=float, required=True,
-                            help='Weight for kldivergence.')
+                            help='Weight for kl-divergence.')
 
     def __init__(self, args: Namespace, dataset: CanomalyDataset):
         super(VAE, self).__init__(args=args, dataset=dataset)
@@ -26,40 +27,34 @@ class VAE(CanomalyModel):
                                            variational=True)
         self.D = get_decoder(args.dataset)(code_length=args.latent_space,
                                            output_shape=dataset.INPUT_SHAPE)
-        self.net = nn.Sequential(self.E, self.D)
+        self.net = nn.Sequential(self.E, self.D).to(device=self.device)
         self.opt = self.Optimizer(self.net.parameters(), **self.optim_args)
         # self.E_opt = self.Optimizer(self.E.parameters(), **self.optim_args)
         # self.D_opt = self.Optimizer(self.D.parameters(), **self.optim_args)
-        self.loss = nn.MSELoss()
+        self.reconstruction_loss = nn.MSELoss()
+
+    def kld_loss(self, mu: torch.Tensor, logvar: torch.Tensor):
+        kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        return kld / (math.prod(self.dataset.INPUT_SHAPE) * self.args.batch_size)
 
     def train_on_batch(self, x: torch.Tensor, y: torch.Tensor, task: int):
         self.opt.zero_grad()
         # encoder forward in variational way
-        encoder_out = self.net[0](x)
+        encoder_out = self.E(x)
         latent_mu, latent_logvar = encoder_out[:x.shape[0], :], encoder_out[x.shape[0]:, :]
-        latent_z = latent_sample(latent_mu, latent_logvar)
-        # decoder forward
-        outputs = self.net[1](latent_z)
-        loss = self.loss(x, outputs) + self.args.beta_kl*kldivergence(latent_mu, latent_logvar)
 
+        # decoder forward
+        latent_z = torch.mul(torch.randn_like(latent_mu), (0.5 * latent_logvar).exp()) + latent_mu
+        outputs = self.D(latent_z)
+
+        loss = self.reconstruction_loss(x, outputs) + self.args.beta_kl*self.kld_loss(latent_mu, latent_logvar)
         loss.backward()
         self.opt.step()
         return loss.item()
 
-    def forward(self, x: torch.Tensor, train: bool = False) -> torch.Tensor:
-        encoder_out = self.net[0](x)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        encoder_out = self.E(x)
         latent_mu, _ = encoder_out[:x.shape[0], :], encoder_out[x.shape[0]:, :]
         # decoder forward
-        outputs = self.net[1](latent_mu)
+        outputs = self.D(latent_mu)
         return outputs
-
-
-def latent_sample(mu: torch.Tensor, logvar: torch.Tensor):
-    # the reparameterization trick
-    std = logvar.mul(0.5).exp_()
-    eps = torch.empty_like(std).normal_()
-    return eps.mul(std).add_(mu)
-
-
-def kldivergence(mu: torch.Tensor, logvar: torch.Tensor):
-    return -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
--- utils/optims.py
+++ utils/optims.py
@@ -3,8 +3,8 @@ from torch.optim import SGD, Adam
 
 
 def add_optimizers_args(parser: ArgumentParser) -> None:
-    parser.add_argument('--optim', type=str, default='SGD',
-                        choices=['SGD', 'Adam'],
+    parser.add_argument('--optim', type=str, default='sgd',
+                        choices=['sgd', 'adam'],
                         help='Optimizer used on the network.')
 
     parser.add_argument('--lr', type=float, required=True,
@@ -12,14 +12,14 @@ def add_optimizers_args(parser: ArgumentParser) -> None:
 
 
 def add_optim_args(optim: str, parser: ArgumentParser) -> None:
-    if optim == 'SGD':
+    if optim == 'sgd':
         return
-    if optim == 'Adam':
+    if optim == 'adam':
         return
 
 
 def get_optim(args: Namespace):
-    if args.optim == 'SGD':
+    if args.optim == 'sgd':
         return SGD, {'lr': args.lr}
-    if args.optim == 'Adam':
+    if args.optim == 'adam':
         return Adam, {'lr': args.lr}
