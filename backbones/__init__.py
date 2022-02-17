f64704667ffccc3a185697ba57a4c9fff7a83e5f
ema-frasca <60889740+ema-frasca@users.noreply.github.com>
2022-02-17 12:57:47 +0100

    forgetting sequence

--- backbones/__init__.py
+++ backbones/__init__.py
@@ -3,16 +3,23 @@ from typing import Type
 from backbones.utils.encoder import Encoder
 from backbones.utils.decoder import Decoder
 
+string_to_erase = ['can-', 'group-', 'forg-']
+
+def replace_str(base: str, to_replace: list[str], replacement: str):
+    text = base
+    for repl in to_replace:
+        text = text.replace(repl, replacement)
+    return text
 
 def get_decoder(dataset: str) -> Type[Decoder]:
-    decoder_name = dataset.replace('can-', '').replace('group-', '').upper() + '_decoder'
+    decoder_name = replace_str(dataset, string_to_erase, '').upper() + '_decoder'
     mod = importlib.import_module('backbones.' + decoder_name)
     class_name = {x.lower(): x for x in mod.__dir__()}[decoder_name.replace('_', '').lower()]
     return getattr(mod, class_name)
 
 
 def get_encoder(dataset: str) -> Type[Encoder]:
-    encoder_name = dataset.replace('can-', '').replace('group-', '').upper() + '_encoder'
+    encoder_name = replace_str(dataset, string_to_erase, '').upper() + '_encoder'
     mod = importlib.import_module('backbones.' + encoder_name)
     class_name = {x.lower(): x for x in mod.__dir__()}[encoder_name.replace('_', '').lower()]
     return getattr(mod, class_name)
--- datasets/forg_fmnist.py
+++ datasets/forg_fmnist.py
@@ -12,31 +12,27 @@ from datasets.utils.canomaly_dataset import CanomalyDataset
 from utils.writer import writer
 
 
-class GroupFMNIST(CanomalyDataset):
-    NAME = 'group-fmnist'
+class ForgFMNIST(CanomalyDataset):
+    NAME = 'forg-fmnist'
     INPUT_SHAPE = (1, 28, 28)
     N_CLASSES = 10
-    N_GROUPS = 4
-    MACRO_CLASSES = [[0, 2, 3, 4, 6], [1], [5, 7, 9], [8]]
+    MACRO_CLASSES = [[7], [5], [1], [8], [0, 2, 3, 4, 6], [9]]
+    N_GROUPS = len(MACRO_CLASSES)
     # MACRO_CLASSES = [[0, 2, 4, 6], [1], [3], [5, 7, 9], [8]]
     ## Classes:
-    ##      0: 0 - 2 - 4 - 6    -> upper body and dresses
-    ##      1: 1 - 3            -> trousers
-    ##      3: 5 - 7 - 9        -> shoes
-    ##      4: 8                -> bags
 
     @staticmethod
     def add_dataset_args(parser: ArgumentParser):
         # parser.add_argument('--classes_per_task', type=int, choices=[1, 2], required=True,
         #                     help='Classes per task. This also determines the number of tasks.')
-        parser.add_argument('--ano_group', type=int, choices=[i for i in range(GroupFMNIST.N_GROUPS)],
-                            default=GroupFMNIST.N_GROUPS-1, help='Group-class that will stay as anomaly.')
+        parser.add_argument('--ano_group', type=int, choices=[i for i in range(ForgFMNIST.N_GROUPS)],
+                            default=ForgFMNIST.N_GROUPS-1, help='Group-class that will stay as anomaly.')
         writer.dir_args.append('ano_group')
         parser.add_argument('--add_rotation', action='store_true', help='Add degrees rotation.')
         parser.add_argument('--min_max_rotation', type=tuple, default=(-15, 15), help='Min max rotation degrees.')
 
     def __init__(self, args: Namespace):
-        super(GroupFMNIST, self).__init__(args)
+        super(ForgFMNIST, self).__init__(args)
 
         self.classes_per_task = 1
         self.n_tasks = self.N_GROUPS - 1
@@ -59,6 +55,19 @@ class GroupFMNIST(CanomalyDataset):
         self.anomaly_group = self.args.ano_group
         self.train_groups = [i for i in range(self.N_GROUPS) if i != self.anomaly_group]
         self.train_quantity = {label: 6000 // len(group) for group in self.MACRO_CLASSES for label in group }
+
+        # for macro_label, group in enumerate(self.MACRO_CLASSES):
+            # test_indexes = torch.isin(self.test_dataset.targets, torch.tensor(group)).nonzero(as_tuple=True)[0]
+            # self.test_dataset.targets[test_indexes] = macro_label
+
+            # n_classes = len(group)
+            # for label in group:
+            #     indexes = (self.train_dataset.targets == label).nonzero(as_tuple=True)[0]
+            #     split_th = 6000 // n_classes
+            #     in_idxes = indexes[:split_th]
+            #     out_idxes = indexes[split_th:]
+            #     self.train_dataset.targets[in_idxes] = macro_label
+            #     self.train_dataset.targets[out_idxes] = self.N_CLASSES
 
 
     def _get_subset(self, labels: List[int], train=True):
