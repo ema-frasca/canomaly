550a830fe335d6d6668865ab90c132868ec93249
rbenaglia <riccardo.benaglia@energyway.it>
2022-03-31 17:21:48 +0200

    add ewc for vae

--- models/rec_vae_ewc.py
+++ models/rec_vae_ewc.py
@@ -7,9 +7,11 @@ from datasets.utils.canomaly_dataset import CanomalyDataset
 import wandb
 from models.rec_vae import RecVAE
 from continual.buffer import Buffer
+from models.utils.recon_model import ReconModel
+from utils.logger import logger
 
 
-# --dataset rec-fmnist --optim adam --lr 0.001 --scheduler_steps 2 --model rec-vae --kl_weight 1 --batch_size 64 --n_epochs 30 --latent_space 32 --approach joint
+# --dataset rec-fmnist --optim adam --lr 0.001 --scheduler_steps 2 --model rec-vae-ewc --batch_size 64 --n_epochs 30 --latent_space 32 --kl_weight 1 --approach continual --e_lambda 1 --gamma 1 --wandb --logs
 class RecVAEEWC(RecVAE):
     NAME = 'rec-vae-ewc'
     VARIATIONAL = True
@@ -25,7 +27,7 @@ class RecVAEEWC(RecVAE):
     def __init__(self, args: Namespace, dataset: CanomalyDataset):
         super().__init__(args=args, dataset=dataset)
 
-        self.fisher = Fisher(self.net, self.opt, self.config)
+        self.fisher = Fisher(self)
 
     def train_on_batch(self, x: torch.Tensor, y: torch.Tensor, task: int):
         self.opt.zero_grad()
@@ -40,22 +42,22 @@ class RecVAEEWC(RecVAE):
         self.opt.step()
         if self.args.wandb:
             wandb.log(
-                {'rec_loss': loss_reconstruction, 'kl_loss': loss_kl * self.args.kl_weight, 'fisher_loss': penalty})
+                {'rec_loss': loss_reconstruction, 'kl_loss': loss_kl * self.args.kl_weight, 'fisher_loss':
+                    penalty, 'fisher_loss_weighted': self.args.e_lambda * penalty})
 
         # logger.autolog_wandb(wandb_yes=self.args.wandb, locals=locals())
         return loss.item()
 
     def train_on_task(self, task_loader: DataLoader, task: int):
-        super().train_on_task(task_loader)
+        super().train_on_task(task_loader, task)
+        self.fisher.update_fish(task_loader)
 
 
 class Fisher:
-    def __init__(self, net, opt, config):
+    def __init__(self, model: ReconModel):
         self.checkpoint = None
         self.fish = None
-        self.net = net
-        self.opt = opt
-        self.config = config
+        self.model = model
 
     @staticmethod
     def add_args(parser: ArgumentParser):
@@ -64,33 +66,48 @@ class Fisher:
 
     def penalty(self) -> torch.Tensor:
         if self.checkpoint is None:
-            return torch.tensor(0.0).to(self.config.device)
+            return torch.tensor(0.0).to(self.model.config.device)
         else:
-            penalty = (self.fish * ((self.net.get_params() - self.checkpoint) ** 2)).sum()
+
+            penalty = (self.fish * ((self.get_params() - self.checkpoint) ** 2)).sum()
             return penalty
 
-    def update_fish(self, dataset):
-        fish = torch.zeros_like(self.net.get_params())
+    def update_fish(self, dl: DataLoader):
+        fish = torch.zeros_like(self.get_params())
 
-        for j, data in enumerate(dataset.train_loader):
-            inputs, labels, _ = data
-            inputs, labels = inputs.to(self.config.device), labels.to(self.config.device)
+        for j, data in enumerate(dl):
+            inputs, labels = data
+            inputs, labels = inputs.to(self.model.config.device), labels.to(self.model.config.device)
             for ex, lab in zip(inputs, labels):
-                self.opt.zero_grad()
-                output = self.net(ex.unsqueeze(0))
-                loss = F.mse_loss(output, lab.unsqueeze(0),
-                                  reduction='none')
+                self.model.opt.zero_grad()
+                output = self.model.forward(ex.unsqueeze(0))
+                rec_loss = self.model.reconstruction_loss(output[0],
+                                                          ex.unsqueeze(0))
+                kld_loss = self.model.kld_not_reduction(*output[1:3])
+                loss = rec_loss + self.model.args.kl_weight * kld_loss
                 detached_loss = torch.mean(loss.detach().clone())
-                loss = torch.mean(loss)
                 loss.backward()
-                fish += detached_loss * self.net.get_grads() ** 2
 
-        fish /= (len(dataset.train_loader) * self.net.args.batch_size)
+                fish += detached_loss * self.get_grads() ** 2
+
+        fish /= (len(dl) * dl.batch_size)
 
         if self.fish is None:
             self.fish = fish
         else:
-            self.fish *= self.net.args.gamma
+            self.fish *= self.model.args.gamma
             self.fish += fish
 
-        self.checkpoint = self.net.get_params().data.clone()
+        self.checkpoint = self.get_params().data.clone()
+
+    def get_params(self):
+        params = []
+        for pp in list(self.model.net.parameters()):
+            params.append(pp.view(-1))
+        return torch.cat(params)
+
+    def get_grads(self):
+        grads = []
+        for pp in list(self.model.net.parameters()):
+            grads.append(pp.grad.view(-1))
+        return torch.cat(grads)
