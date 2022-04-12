e50b6e07b7f4f8d2577594d9067427b95e988ebb
rbenaglia <riccardo.benaglia@energyway.it>
2022-04-12 18:49:43 +0200

    add celeba and cifar

--- datasets/rec_celeba.py
+++ datasets/rec_celeba.py
@@ -1,39 +1,39 @@
-from typing import List
-
-import numpy as np
 import torch
 from argparse import Namespace, ArgumentParser
-from torchvision.datasets import CIFAR10
+from torchvision.datasets import CelebA
 from torchvision import transforms
 from torch.utils.data import Subset, DataLoader
+from typing import List
 
-from datasets.transforms.rotation import Rotation
 from datasets.utils.canomaly_dataset import CanomalyDataset
-from utils.writer import writer
+from itertools import product
 
 
-class RecCIFAR10(CanomalyDataset):
-    NAME = 'rec-cifar10'
-    INPUT_SHAPE = (3, 32, 32)
-    N_CLASSES = 10
-    # N_CLASSES = 4
-    # MACRO_CLASSES = [[0, 2, 3, 4, 6], [1], [5, 7, 9], [8]]
-    MACRO_CLASSES = [[0],
-                     [1],
-                     [2],
-                     [3],
-                     [4], [5], [6], [7], [8], [9]
-                     ]
-    # MACRO_CLASSES = [[0], [1]]
+class MyCeleba(CelebA):
+    def __init__(self, target_idxes, **kwargs):
+        super().__init__(**kwargs)
+        self.target_idxes = target_idxes
+        targets = product([0, 1], repeat=len(target_idxes))
+        self.trans_dict = {t: num for num, t in enumerate(targets)}
+        self.targets = [self.trans_dict[tuple(x.tolist())] for x in self.attr[:, target_idxes]]
+
+    def __getitem__(self, item):
+        X, target = super().__getitem__(item)
+        real_target = self.trans_dict[tuple(target[self.target_idxes].tolist())]
+        return X, real_target
+
+
+class RecCelebA(CanomalyDataset):
+    NAME = 'rec-celeba'
+    IMG_SIZE = 64
+    INPUT_SHAPE = (3, IMG_SIZE, IMG_SIZE)
+    TARGET_IDXES = [9, 20]
+    N_CLASSES = len([x for x in product([0, 1], repeat=len(TARGET_IDXES))])
+    MACRO_CLASSES = [[i] for i in range(N_CLASSES)]
     N_GROUPS = len(MACRO_CLASSES)
-
-    # MACRO_CLASSES = [[0, 2, 4, 6], [1], [3], [5, 7, 9], [8]]
-    ## Classes:
 
     @staticmethod
     def add_dataset_args(parser: ArgumentParser):
-        parser.add_argument('--add_rotation', action='store_true', help='Add degrees rotation.')
-        parser.add_argument('--min_max_rotation', type=tuple, default=(-15, 15), help='Min max rotation degrees.')
         parser.add_argument('--poison_perc', default=0, type=float)
 
     def __init__(self, args: Namespace):
@@ -44,37 +44,36 @@ class RecCIFAR10(CanomalyDataset):
         if args.approach == 'joint' and not args.per_task:
             self.n_tasks = 1
 
-        transform_list = []
-        if args.add_rotation:
-            deg_min = args.min_max_rotation[0] if args.min_max_rotation is not None else -90
-            deg_max = args.min_max_rotation[1] if args.min_max_rotation is not None else 90
-            transform_list.append(Rotation(deg_min=deg_min, deg_max=deg_max))
-
-        common_transform = [transforms.ToTensor(),
-                            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2615))]
-
-        transform_list.extend(common_transform)
-
+        transform_list = [
+            transforms.Resize(self.IMG_SIZE),
+            # transforms.RandomHorizontalFlip(),
+            transforms.CenterCrop(self.IMG_SIZE),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
         self.transform = transforms.Compose(transform_list)
 
-        self.transform_test = transforms.Compose(common_transform)
-        self.target_transform = transforms.Compose([])
-        self.train_dataset = CIFAR10(self.config.data_dir, train=True, download=True, transform=self.transform,
-                                     target_transform=self.target_transform)
-        self.test_dataset = CIFAR10(self.config.data_dir, train=False, download=True, transform=self.transform_test,
-                                    target_transform=self.target_transform)
+        # if ssl doesn't work:
+        # import ssl
+        # ssl._create_default_https_context = ssl._create_unverified_context
+
+        self.train_dataset = MyCeleba(root=self.config.data_dir, split='train', download=True, transform=self.transform,
+                                      target_idxes=self.TARGET_IDXES)
+        self.test_dataset = MyCeleba(root=self.config.data_dir, split='test', download=True, transform=self.transform,
+                                     target_idxes=self.TARGET_IDXES)
 
         self.train_groups = [i for i in range(self.N_GROUPS)]
-        # train max: 6k | test max: 1k
-        self.train_quantity = {label: -1  # 6000 // (len(group))
-                               for group in self.MACRO_CLASSES for label in group}
-        self.test_quantity = {label: 300 // (len(group))
+        self.train_quantity = {label: 5000 // len(group) for group in self.MACRO_CLASSES for label in group}
+        self.test_quantity = {label: -1  # 1000 // (len(group))
                               for group in self.MACRO_CLASSES for label in group}
+
         self.classes_seen = []
 
     def _get_subset(self, labels: List[int], train=True):
         self.classes_seen.append(labels)
         self.last_seen_classes = labels
+        # idxes = (self.train_dataset.attr[:, 20] == 1).nonzero(as_tuple=True)[0]
+        # return Subset(self.train_dataset, idxes)
         base_ds = self.train_dataset if train else self.test_dataset
         index_list = []
         for label in labels:
@@ -93,9 +92,12 @@ class RecCIFAR10(CanomalyDataset):
             yield self._get_subset(self.MACRO_CLASSES[group_idx])
 
     def _get_test_dataset(self):
+        # idxes = (self.test_dataset.attr[:, 20] == 1).nonzero(as_tuple=True)[0]
+        # return Subset(self.train_dataset, idxes)
         index_list = []
         for label in range(self.N_CLASSES):
-            idxes = (torch.Tensor(self.test_dataset.targets) == label).nonzero(as_tuple=True)[0][:self.test_quantity[label]]
+            idxes = (torch.Tensor(self.test_dataset.targets) == label).nonzero(as_tuple=True)[0][
+                    :self.test_quantity[label]]
             index_list.append(idxes)
         idxes = torch.cat(index_list)
         return Subset(self.test_dataset, idxes)
